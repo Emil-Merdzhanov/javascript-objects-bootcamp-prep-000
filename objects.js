1525cf02839d5b34cb4e54f988b83ae9a4645b9e
decent-port-4955 <emil@georgetownoliveoil.com>
2019-09-21 03:39:52 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,22 +1,5 @@
-var recipes = new Object();
+var playlist = new Object ({artist:'name'})
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]: value})
-}
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
- object[key] = value
-return object
-}
-
-function deleteFromObjectByKey(object, key){
-  var newObj =  Object.assign({},object)
-  delete newObj[key]
-  return newObj
-
-}
-
-function destructivelyDeleteFromObjectByKey(object, key){
-  delete object[key]
-  return object
 }
