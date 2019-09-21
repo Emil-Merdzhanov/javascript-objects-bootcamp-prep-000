c4485d83ab91960cbaf911024e7b6058b7cc87d9
decent-port-4955 <emil@georgetownoliveoil.com>
2019-09-21 03:39:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object ({artist:'name'})
+var playlist = new Object ()
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({},object,{[key]: value})
